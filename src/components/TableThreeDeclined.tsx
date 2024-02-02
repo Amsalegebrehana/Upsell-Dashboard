3138904b76edd3765bce389443bcad271044946a
Amsalegebrehana <Se.amsale.gebrehana@gmail.com>
2024-02-02 23:27:06 +0300

    Tables for upsell requests edited

--- src/components/TableThreeDeclined.tsx
+++ src/components/TableThreeDeclined.tsx
@@ -1,5 +1,5 @@
 import { Checkbox } from "@material-tailwind/react";
-const TableThree = () => {
+const TableThreeDeclined = () => {
   return (
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
@@ -360,4 +360,4 @@ const TableThree = () => {
   );
 };
 
-export default TableThree;
+export default TableThreeDeclined;
